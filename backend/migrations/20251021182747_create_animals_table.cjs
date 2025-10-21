c42c43d5673801cf83d5de60542b3c72aa9aa188
platovich <platovich@yandex.ru>
2025-10-21 21:45:14 +0300

    feat: add migrations for animals and events table

--- backend/migrations/20251021182747_create_animals_table.cjs
+++ backend/migrations/20251021182747_create_animals_table.cjs
@@ -1,8 +1,16 @@
 
-exports.up = function(knex) {
-  
+// backend/migrations/20251021182747_create_animals_table.cjs
+// Migration to create the "animals" table
+
+exports.up = async function (knex) {
+    await knex.schema.createTable("animals", (table) => {
+        table.increments("id").primary();
+        table.string("name").notNullable();
+        table.string("species").notNullable();
+        table.date("birth_date");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+    await knex.schema.dropTableIfExists("animals");
 };
--- backend/migrations/20251021182813_create_events_table.cjs
+++ backend/migrations/20251021182813_create_events_table.cjs
@@ -1,8 +1,15 @@
-
-exports.up = function(knex) {
-  
+// backend/migrations/20251021182813_create_events_table.cjs
+// Migration to create the "events" table
+exports.up = async function(knex) {
+  await knex.schema.createTable("events", (table) => {
+    table.increments("id").primary();
+    table.integer("animal_id").notNullable().references("id").inTable("animals").onDelete("CASCADE");
+    table.string("type").notNullable();
+    table.string("description").notNullable();
+    table.timestamp("event_date").notNullable().defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("events");    
 };
